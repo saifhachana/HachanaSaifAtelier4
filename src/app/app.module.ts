207862d38eb8f17d53e8a7b2398770ac23d01d4e
Hachana Saif <saifhachana@gmail.com>
2020-11-27 20:57:37 +0100

    Atelier 4 initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AccueilComponent } from './composants/accueil/accueil.component';
-import { ListmaterielComponent } from './composants/listmateriel/listmateriel.component';
+import { ListmaterielComponent } from './composants/informatique/listmateriel/listmateriel.component';
 
 @NgModule({
   declarations: [
