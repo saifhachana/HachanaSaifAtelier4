49e5be8d551805eed7668583a75280402576dfb0
Hachana Saif <saifhachana@gmail.com>
2020-11-27 21:57:43 +0100

    apres modification

--- fsdf/src/app/app.module.ts
+++ fsdf/src/app/app.module.ts
@@ -4,8 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AccueilComponent } from './composants/accueil/accueil.component';
-import { ListmaterielComponent } from './composants/informatique/listmateriel/listmateriel.component';
-import { MaterielComponent } from './composants/informatique/materiel/materiel.component';
+import { ListmaterielComponent } from './composants/listmateriel/listmateriel.component';
+import { MaterielComponent } from './composants/materiel/materiel.component';
 
 @NgModule({
   declarations: [
